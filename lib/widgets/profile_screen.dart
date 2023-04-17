9f4afd99d313c347b7b62ed6a39c92ce6f750979
admin <admin@example.com>
2023-04-17 06:48:36 +0100

    changes.

--- lib/widgets/profile_screen.dart
+++ lib/widgets/profile_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
 import 'application_appbar.dart';
+import "package:google_maps_flutter/google_maps_flutter.dart";
+import "package:location/location.dart";
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({
-    super.key,
-  });
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  late LocationData _currentLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+  }
+
+  Future<void> _getLocation() async {
+    final location = Location();
+
+    try {
+      final currLocation = await location.getLocation();
+      setState(() {
+        _currentLocation = currLocation;
+      });
+    } catch (e) {}
+  }
 
   @override
   Widget build(BuildContext context) {
